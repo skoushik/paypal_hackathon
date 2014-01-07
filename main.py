a4375a797f18dc1466826674e3463b49c913b848
skoushik <skoushik333@gmail.com>
2014-01-07 06:12:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 from selenium import webdriver
 from SimpleCV import Camera, Display
+import selenium.webdriver.support.ui as ui
 
 cam = Camera()
-disp = Display()
-
-while disp.isNotDone():
-	img = cam.getImage()
-	img.show()
-	if disp.mouseLeft:
-		break
+img = cam.getImage()
+wait = ui.WebDriverWait(driver, 10)
 
 driver = webdriver.Firefox()
 driver.get("http://images.google.com")
-driver.find_element_by_xpath('//*[@id="gs_st0"]/a[1]').click
-driver.find_element_by_xpath('//*[@id="qbug"]/div/a').click
-driver.find_element_by_xpath('//*[@id="qbfile"]').click
+driver.find_element_by_xpath('//*[@id="gs_st0"]/a[1]').click()
+driver.find_element_by_xpath('//*[@id="qbug"]/div/a').click()
+driver.find_element_by_xpath('//*[@id="qbfile"]').click()
+results = wait.until(lambda driver : driver.find_element_by_xpath('//*[@id="gbqfq"]'))
 text = driver.find_element_by_xpath('//*[@id="gbqfq"]')
 
 driver.get('http://www.eBay.in')
 driver.find_element_by_xpath('//*[@id="gh-ac"]').send_keys(text)
-
